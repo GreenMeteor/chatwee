04b48f11275f50cdb301cfb1e5d46873ff4867da
Çağatay <cagatayldzz@gmail.com>
2018-01-12 09:47:59 +0300

    Create base.php

---
+++ messages/tr/base.php
@@ -0,0 +1,8 @@
+<?php
+
+    return [
+        'ChatWee Settings' => 'ChatWee Ayarlar',
+        'ChatWee Widget URL:' => 'ChatWee Widget URL:',
+        '<strong>ChatWee</strong> module configuration' => '<strong>ChatWee</strong> modül yapılandırması',
+        'Save' => 'Kaydet',
+    ];
