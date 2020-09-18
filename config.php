773f76035ad5b07847d2eb76bc6b193e2ceac69a
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-09-18 19:27:27 -0400

    Enh: coding standards

--- config.php
+++ config.php
@@ -2,35 +2,18 @@
 
 namespace humhub\modules\chatwee;
 
+use humhub\widgets\LayoutAddons;
+use humhub\modules\chatwee\Events;
+use humhub\modules\chatwee\Module;
+use humhub\modules\admin\widgets\AdminMenu;
+
 return [
     'id' => 'chatwee',
-    'class' => 'humhub\modules\chatwee\Module',
+    'class' => Module::class,
     'namespace' => 'humhub\modules\chatwee',
     'events' => [
-        [
-            'class' => \humhub\modules\dashboard\widgets\Sidebar::class,
-            'event' => \humhub\modules\dashboard\widgets\Sidebar::EVENT_INIT,
-            'callback' => [
-                'humhub\modules\chatwee\Events',
-                'addChatweeFrame'
-            ]
-        ],
-        [
-            'class' => \humhub\modules\space\widgets\Sidebar::class,
-            'event' => \humhub\modules\space\widgets\Sidebar::EVENT_INIT,
-            'callback' => [
-                'humhub\modules\chatwee\Events',
-                'addChatweeFrame'
-            ]
-        ],
-        [
-            'class' => \humhub\modules\admin\widgets\AdminMenu::class,
-            'event' => \humhub\modules\admin\widgets\AdminMenu::EVENT_INIT,
-            'callback' => [
-                'humhub\modules\chatwee\Events',
-                'onAdminMenuInit'
-            ]
-        ]
+        ['class' => LayoutAddons::class, 'event' => LayoutAddons::EVENT_INIT, 'callback' => [Events::class,'onLayoutAddonsInit']],
+        ['class' => AdminMenu::class,'event' => AdminMenu::EVENT_INIT, 'callback' => [Events::class, 'onAdminMenuInit']]
     ]
 ];
 ?>
